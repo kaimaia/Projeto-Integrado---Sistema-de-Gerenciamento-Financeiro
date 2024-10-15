3873e9f6ff934cdddeb7405f63b29e2fb9e29360
Ricken Diniz <87033966+ricken-diniz@users.noreply.github.com>
2024-10-15 10:22:05 -0300

    Raiz do projeto

---
+++ gerenciamento_de_finanças/app/Http/Controllers/RelacionadoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Relacionado;
+use Illuminate\Http\Request;
+
+class RelacionadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Relacionado  $relacionado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Relacionado $relacionado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Relacionado  $relacionado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Relacionado $relacionado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Relacionado  $relacionado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Relacionado $relacionado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Relacionado  $relacionado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Relacionado $relacionado)
+    {
+        //
+    }
+}
