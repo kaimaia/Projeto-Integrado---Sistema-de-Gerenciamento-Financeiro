37942832cd6261923ca21b0d157cb85c0dccd2fd
Ricken Diniz <87033966+ricken-diniz@users.noreply.github.com>
2024-10-16 11:40:53 -0300

    Add files via upload

--- gerenciamento_de_finanças/database/migrations/2024_10_15_131501_create_usuarios_table.php
+++ gerenciamento_de_finanças/database/migrations/2024_10_15_131501_create_usuarios_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('nome');
+            $table->string('email');
+            $table->string('senha');
+            $table->string('funcao');
         });
     }
 
